4f24c4c1ccb455aa01d102427bdcc386ca6ad12c
Connie Luty <connieluty@Connies-iMac.local>
2020-04-28 16:02:38 -0500

    Weapon pick ups work

---
+++ DeathMatch/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool canPickUp;
+    private GameObject player;
+    private int health;
+    private Transform hand;
+    public GameObject lastTouchedWeapon;
+    public bool holdingWeapon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 5;
+        player = this.gameObject;
+        holdingWeapon = false;
+        canPickUp = false;
+        hand = this.transform.GetChild(0).GetChild(0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canPickUp)
+        {
+            PickUp();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I have ran into something");
+        if( other.gameObject.CompareTag("earth") ||
+            other.gameObject.CompareTag("wind") ||
+            other.gameObject.CompareTag("water") ||
+            other.gameObject.CompareTag("fire") )
+        {
+            lastTouchedWeapon = other.gameObject;
+            canPickUp = true;
+        }
+    }
+
+    void PickUp(){
+        Debug.Log("WEAPONS!!!");
+        lastTouchedWeapon.transform.SetParent(hand);
+        lastTouchedWeapon.transform.localPosition = Vector3.zero;
+        lastTouchedWeapon.transform.localRotation = Quaternion.identity;
+    }
+}
