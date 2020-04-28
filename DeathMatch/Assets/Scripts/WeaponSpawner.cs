43f485af74ce5adc41eb1f71366611c361e6b220
Connie Luty <connieluty@Connies-iMac.local>
2020-04-28 14:44:08 -0500

    Cleaned up. Ready to begin.

---
+++ DeathMatch/Assets/Scripts/WeaponSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpawner : MonoBehaviour
+{
+    public List<string> weaponNames;
+    public List<GameObject> rifles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRifle());
+    }
+
+    IEnumerator SpawnRifle(){
+        while(true){
+            GameObject newGun = Instantiate(rifles[Random.Range(0, rifles.Count)], this.transform.position, this.transform.rotation);
+            newGun.name = weaponNames[Random.Range(0, weaponNames.Count)];
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
